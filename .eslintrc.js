5b8cf064e597e14411d3cc69fb1bfb093cbdfc13
exnight <exnight@users.noreply.github.com>
2019-02-12 21:51:35 +0800

    update config and file structure

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.export = {
+  "root": true,
+  "env": {
+    "es6": true,
+    "node": true
+  },
+  "extends": "airbnb",
+  "parserOptions": {
+    "ecmaVersion": 2018
+  },
+  "plugins": [
+      "react",
+      "jsx-a11y",
+      "import"
+    ],
+  "rules": {
+    "react/jsx-filename-extension": ["error", {
+      "extensions": [".js", ".jsx"]
+  }],
+    "linebreak-style": ["error", "windows"],
+    "no-template-curly-in-string": 0,
+    "react/jsx-max-props-per-line": 0,
+    "object-curly-newline": 0,
+  },
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const merge = require('webpack-merge');
 
-const common = require('./webpack.config.common');
-const dev = require('./webpack.config.dev');
+const common = require('./config/webpack.config.common');
+const dev = require('./config/webpack.config.dev');
 
 module.exports = merge.smart(common, dev);
