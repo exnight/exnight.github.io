3b27dd56142e82f4a937e0038cf2a729488ad802
exnight <exnight@users.noreply.github.com>
2020-05-23 17:03:57 +0800

    update footer

--- src/components/app/footer/footer.tsx
+++ src/components/app/footer/footer.tsx
@@ -1,16 +1,34 @@
 import React from 'react';
 
-import { Typography } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import { Box, Typography } from '@material-ui/core';
 
 import CopyrightIcon from '@material-ui/icons/Copyright';
 import PersonIcon from '@material-ui/icons/Person';
 import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
 import MenuBookIcon from '@material-ui/icons/MenuBook';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    footer: {
+      padding: 10,
+      position: 'absolute',
+      textAlign: 'center',
+      left: 0,
+      right: 0,
+      bottom: 0,
+      backgroundColor: theme.palette.secondary.main,
+      color: theme.palette.secondary.contrastText,
+    },
+  })
+);
+
 const Footer: React.FC = () => {
+  const classes = useStyles();
+
   return (
-    <footer>
-      <Typography align="center">
+    <Box className={classes.footer}>
+      <Typography>
         Copyright
         <CopyrightIcon fontSize="large" />
         Leo
@@ -20,7 +38,7 @@ const Footer: React.FC = () => {
         2016 - 2020 | <MenuBookIcon fontSize="large" />
         Not Powered By WordPressヽ(￣▽￣)ノ
       </Typography>
-    </footer>
+    </Box>
   );
 };
 
