4af406146ff53d43893a621ca22b7f53eb1e8dc8
exnight <exnight@users.noreply.github.com>
2020-05-29 10:24:50 +0800

    add skills

--- src/routes/about/components/skill/skillList.tsx
+++ src/routes/about/components/skill/skillList.tsx
@@ -1,25 +1,43 @@
 import React from 'react';
 
 import Grid from '@material-ui/core/Grid';
-import LinearProgress from '@material-ui/core/LinearProgress';
 import Typography from '@material-ui/core/Typography';
 
-// const skillProficient = [];
-// const skillFamiliar = [];
+import SkillItem from './skillItem';
+
+interface SkillContent {
+  skill: string;
+  value: number;
+}
+
+const programmingLang: SkillContent[] = [
+  { skill: 'Python', value: 85 },
+  { skill: 'ReactJS', value: 75 },
+  { skill: 'C++', value: 50 },
+  { skill: 'Java', value: 50 },
+];
+
+const tools: SkillContent[] = [
+  { skill: 'git', value: 75 },
+  { skill: 'Docker', value: 50 },
+  { skill: 'Keras', value: 70 },
+  { skill: 'LaTeX', value: 60 },
+  { skill: 'sklearn', value: 80 },
+  { skill: 'Tensorflow', value: 60 },
+];
 
 const SkillList: React.FC = () => {
   return (
     <Grid container direction="column">
-      <Grid item>
-        <Typography variant="h6">s1</Typography>
-        <LinearProgress variant="determinate" value={75} />
-      </Grid>
-      <Grid item>
-        <Typography variant="h6">s2</Typography>
-      </Grid>
-      <Grid item>
-        <Typography variant="h6">s3</Typography>
-      </Grid>
+      <Typography variant="body1">
+        <em>Programming Languages</em>
+      </Typography>
+      <SkillItem data={programmingLang} />
+
+      <Typography variant="body1">
+        <em>Libraries and Tools</em>
+      </Typography>
+      <SkillItem data={tools} />
     </Grid>
   );
 };
