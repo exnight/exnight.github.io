b5a1ab6a24fc7ade08e93cebb29054674d041957
exnight <exnight@users.noreply.github.com>
2021-05-15 19:40:41 +0800

    implement new footer

--- src/components/app/footer/footer.tsx
+++ src/components/app/footer/footer.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 const Footer: React.FC = () => {
   return (
-    <>
-      Copyright Leo
-      <em> exnight </em>
-      Wong | HKUST | 2016 - 2020 | Not Powered By WordPressヽ(￣▽￣)ノ
-    </>
+    <footer className="h-24 bg-gray-800 text-lg text-white mt-12 py-8">
+      <p className="text-center">
+        &copy; Leo
+        <em> exnight </em>
+        Wong | 2016 - 2021
+      </p>
+    </footer>
   );
 };
 
