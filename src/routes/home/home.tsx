8d3bc6b8c7989943da6b6d7d44d2417a11b2435a
exnight <exnight@users.noreply.github.com>
2021-05-15 18:57:08 +0800

    preliminary home page (no RWD)

--- src/routes/home/home.tsx
+++ src/routes/home/home.tsx
@@ -4,30 +4,61 @@ import Typed from 'react-typed';
 
 const Home: React.FC = () => {
   const subtitleContent = [
-    'Quantitative Finance',
-    'Computer Science',
-    'Quant Developer',
+    'Software Engineer',
+    'Basketball Lover',
+    'CSGO Tactician',
   ];
   const typingForwardSpeed = 40;
   const typingBackwardSpeed = 40;
 
   return (
-    <>
-      <Typed
-        strings={[`Leo WONG`, `<em>exnight</em>`]}
-        typeSpeed={typingForwardSpeed}
-        backSpeed={typingBackwardSpeed}
-        backDelay={4000}
-        loop
-      />
-      <Typed
-        strings={subtitleContent}
-        typeSpeed={typingForwardSpeed}
-        backSpeed={typingBackwardSpeed}
-        backDelay={2000}
-        loop
-      />
-    </>
+    <div className="h-full w-4/5 flex flex-col space-y-16 container">
+      <div className="grid grid-cols-2 pt-16 px-8">
+        <div>
+          <h1 className="text-4xl pb-8">Hi,</h1>
+          <div className="grid grid-cols-4 gap-4 items-center pb-8">
+            <div className="text-lg">I'm Leo, a</div>
+            <div className="col-span-3">
+              {/* use another font */}
+              <Typed
+                strings={subtitleContent}
+                typeSpeed={typingForwardSpeed}
+                backSpeed={typingBackwardSpeed}
+                backDelay={3000}
+                loop
+                className="text-center text-4xl text-blue-600"
+              />
+            </div>
+          </div>
+          <div>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero
+            delectus fugiat libero debitis aperiam unde ipsum non veniam culpa,
+            eveniet, mollitia illum, cupiditate at ad magnam necessitatibus
+            officiis magni fugit?
+          </div>
+        </div>
+        <div className="flex flex-col items-center justify-center space-y-8">
+          <img
+            src="https://picsum.photos/200/200"
+            alt=""
+            className="h-48 rounded-full"
+          />
+          <div className="text-4xl anurati-font tracking-widest">EXPLORE</div>
+        </div>
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ipsam a
+        quaerat saepe voluptatum aut reprehenderit modi illum deleniti tenetur?
+        Quas in perspiciatis quos deleniti quaerat id officiis. Explicabo,
+        atque?
+      </div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ipsam a
+        quaerat saepe voluptatum aut reprehenderit modi illum deleniti tenetur?
+        Quas in perspiciatis quos deleniti quaerat id officiis. Explicabo,
+        atque?
+      </div>
+    </div>
   );
 };
 
