0688e52122616cc16d9e1d7a43abe9f48a1d2c16
exnight <exnight@users.noreply.github.com>
2021-05-16 19:26:16 +0800

    update footer content

--- src/components/app/footer/footer.tsx
+++ src/components/app/footer/footer.tsx
@@ -1,14 +1,24 @@
 import React from 'react';
 
 const Footer: React.FC = () => {
-  // ? add more content
+  // TODO: use icons for media links
   return (
-    <footer className="h-24 bg-gray-800 text-lg text-white mt-12 py-8">
-      <p className="text-center">
-        &copy; Leo
-        <em> exnight </em>
-        Wong | 2016 - 2021
-      </p>
+    <footer className="bg-gray-800 text-lg text-white mt-8 py-8 inline-block">
+      <div className="w-4/5 mx-auto justify-between items-center flex flex-col space-y-4 sm:space-y-0 sm:flex-row">
+        <p className="text-center">
+          &copy; Leo
+          <em> exnight </em>
+          Wong | 2016 - 2021
+        </p>
+        <ul className="text-center flex space-x-8">
+          <li>
+            <a href="https://github.com/exnight">GitHub</a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/jyleowong/">LinkedIn</a>
+          </li>
+        </ul>
+      </div>
     </footer>
   );
 };
