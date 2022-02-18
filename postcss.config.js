d661894a055f342166e13d041095c642ac238e20
exnight <exnight@users.noreply.github.com>
2022-02-18 17:08:30 +0800

    update configuration

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,3 @@
-/* eslint-disable global-require */
-
-const cssnano = require('cssnano');
-
 module.exports = {
-  plugins: [
-    require('postcss-import'),
-    require('tailwindcss'),
-    require('postcss-nested'),
-    process.env.NODE_ENV === 'production' ? require('autoprefixer') : null,
-    process.env.NODE_ENV === 'production' ? cssnano() : null,
-  ],
+  plugins: ['tailwindcss', 'postcss-preset-env'],
 };
