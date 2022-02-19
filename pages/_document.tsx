5788b41a3cbf555b7353db428b30979055a669ee
exnight <exnight@users.noreply.github.com>
2022-02-19 20:24:00 +0800

    include blog dependencies

---
+++ pages/_document.tsx
@@ -0,0 +1,22 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+const Doucument = () => {
+  return (
+    <Html>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/katex@0.15.2/dist/katex.min.css"
+          integrity="sha384-MlJdn/WNKDGXveldHDdyRP1R4CTHr3FeuDNfhsLPYrq2t0UBkUdK2jyTnXPEK1NQ"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+};
+
+export default Doucument;
