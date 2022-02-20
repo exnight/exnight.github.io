a56f9582672f97e2a78ee7575a8ac9e7b8ba5622
exnight <exnight@users.noreply.github.com>
2022-02-20 12:41:30 +0800

    import cleanup

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,13 +1,11 @@
-import React from 'react';
 import Footer from './Footer';
-import styles from './Layout.module.css';
 import Navigation from './Navigation';
 
 const Layout: React.FC = ({ children }) => {
   return (
     <>
       <Navigation />
-      <main className={styles.main}>{children}</main>
+      <main style={{ minHeight: '80vh' }}>{children}</main>
       <Footer />
     </>
   );
