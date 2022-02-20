97c868c96c47cd9207a0813f1beeaac1f844ecfc
exnight <exnight@users.noreply.github.com>
2022-02-20 13:55:19 +0800

    switch to preact

--- next.config.js
+++ next.config.js
@@ -6,6 +6,18 @@ const nextConfig = {
     loader: 'akamai',
     path: '/',
   },
+  webpack: (config, options) => {
+    if (!options.dev && !options.isServer) {
+      Object.assign(config.resolve.alias, {
+        // assign react/jsx-runtime.js to preact/jsx-runtime
+        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
+    return config;
+  },
 };
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
