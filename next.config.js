e8c78497e35d4c0f2971471f359a595aa4601316
exnight <exnight@users.noreply.github.com>
2023-01-20 20:27:19 +0800

    drop preact

--- next.config.js
+++ next.config.js
@@ -6,17 +6,6 @@ const nextConfig = {
     loader: 'akamai',
     path: '/',
   },
-  // webpack: (config, options) => {
-  //   if (!options.dev && !options.isServer) {
-  //     Object.assign(config.resolve.alias, {
-  //       'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-  //       react: 'preact/compat',
-  //       'react-dom/test-utils': 'preact/test-utils',
-  //       'react-dom': 'preact/compat', // Must be below test-utils
-  //     });
-  //   }
-  //   return config;
-  // },
 };
 
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
