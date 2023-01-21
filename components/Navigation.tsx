56d4c3ce8fb0829a9af4fb42a88a6423cd45e857
Leo Wong <exnight@users.noreply.github.com>
2023-01-21 02:39:12 +0000

    fix build and use Next.js 13 (#34)
    * drop preact
    
    * use Next.js v13

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -4,12 +4,8 @@ const Navigation: React.FC = () => {
   return (
     <div className="my-4 pb-4 border-b">
       <div className="justify-items-center items-center mx-auto sm:w-4/5 grid grid-cols-3 md:grid-cols-5 lg:grid-cols-8">
-        <Link href="/">
-          <a>Home</a>
-        </Link>
-        <Link href="/blog">
-          <a>Blog</a>
-        </Link>
+        <Link href="/">Home</Link>
+        <Link href="/blog">Blog</Link>
       </div>
     </div>
   );
