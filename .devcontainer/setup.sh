e53bb1437f86b080341e6f01168f7ed6470de263
exnight <exnight@users.noreply.github.com>
2024-11-20 21:51:47 +0800

    drop Lazygit

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,12 +1,5 @@
 #!/bin/sh
 
-# Copy all configuration files
-mkdir -p ~/.config
-cp -r ./config/* ~/.config
-
-# Install Lazygit
-./.devcontainer/install-lazygit.sh
-
 # Disable telemetry
 echo
 npx next telemetry disable
