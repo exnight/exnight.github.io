e79b7e7a032ead65212cb299e719eec676bd1904
exnight <exnight@users.noreply.github.com>
2024-11-23 11:12:37 +0000

    downgrade Tailwind to v3

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,20 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
+import { purgeCss } from 'vite-plugin-tailwind-purgecss'
+import { analyzer } from 'vite-bundle-analyzer'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss()],
+  plugins: [
+    vue(),
+    vueDevTools(),
+    purgeCss(),
+    analyzer({
+      analyzerMode: 'static',
+      defaultSizes: 'gzip',
+    }),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
