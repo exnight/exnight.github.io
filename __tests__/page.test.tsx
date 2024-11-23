b155dc6dfd58e5e0d82d324b923e825b9ad70bc3
exnight <exnight@users.noreply.github.com>
2024-11-23 03:10:21 +0000

    use Vue

--- __tests__/page.test.tsx
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from "vitest";
-import { render, screen } from "@testing-library/react";
-import Home from "../app/page";
-
-test("Render Home page", async () => {
-  render(<Home />);
-  screen.getByText("Save and see your changes instantly.");
-});
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(), vueDevTools()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  server: {
+    port: 3000,
+    host: '127.0.0.1',
+    watch: {
+      usePolling: true,
+      interval: 5000,
+      binaryInterval: 5000,
+    },
+  },
+})
