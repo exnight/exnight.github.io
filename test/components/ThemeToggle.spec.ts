c6c3d73252f92b5a5d9704dfd80ce846916368eb
exnight <exnight@users.noreply.github.com>
2024-12-10 14:27:11 +0000

    update tests

--- test/components/ThemeToggle.spec.ts
+++ test/components/ThemeToggle.spec.ts
@@ -9,12 +9,10 @@ it('can mount ThemeToggle', async () => {
   })
 
   const toDark = wrapper.get('button').get('#to-dark')
-  expect(toDark.text()).toEqual('To Dark')
   expect(toDark.classes()).toContain('inline')
   expect(toDark.classes()).toContain('dark:hidden')
 
   const toLight = wrapper.get('button').get('#to-light')
-  expect(toLight.text()).toEqual('To Light')
   expect(toLight.classes()).toContain('hidden')
   expect(toLight.classes()).toContain('dark:inline')
 })
