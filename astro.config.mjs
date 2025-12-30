fcfeb1f54810717de226a4d05b6920ce10761a88
exnight <exnight@users.noreply.github.com>
2025-12-30 11:46:08 +0000

    add KaTeX

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 import mdx from '@astrojs/mdx';
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
-  integrations: [mdx()],
+  integrations: [mdx({ remarkPlugins: [remarkMath], rehypePlugins: [rehypeKatex] })],
   markdown: {
     shikiConfig: {
       themes: {
